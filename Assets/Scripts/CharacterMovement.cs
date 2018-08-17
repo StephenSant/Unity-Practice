3bca6f44778458ca2f40ffbf95da94649c24489f
StephenSant <42423953+StephenSant@users.noreply.github.com>
2018-08-17 16:28:32 +1000

    17/8/18

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -5,40 +5,35 @@ using UnityEngine;
 public class CharacterMovement : MonoBehaviour
 {
     #region Variables
-    public bool isPaused;
     [Header("PLAYER VARIABLES")]
     [Header("Movement Variables")]
-    public float speed = 6f;
-    public float jumpSpeed = 8f;
-    public float gravity = 20f;
-    private Vector3 moveDirection = Vector3.zero;
-    private CharacterController controller;
-    public GameObject myCamera;
+    public float speed = 6f;//how fast the player can move
+    public float jumpSpeed = 8f;//how height the player can jump
+    public float gravity = 20f;//player's gravity
+    private Vector3 moveDirection = Vector3.zero;//direction the player is moving
+    private CharacterController controller;//player controller component
+    private Vector3 spawnPoint;//sets spawnpoint
+    public bool isPaused;//weather or not the game is paused
     [Header("CAM ROTATION VARIABLES")]
     [Header("Rotational Axis")]
-    //create a public link to the rotational axis called axis and set a defualt axis
-    public RotationalAxis axis = RotationalAxis.MouseX;
-    [Header("Sensitivity")]
-    //public floats for our x and y sensitivity
+    public GameObject myCamera;//Gets the camera
+    float rotationY = 0.0f;//float for rotation Y
+    public RotationalAxis axis = RotationalAxis.MouseX;//create a public link to the rotational axis called axis and set a defualt axis
+    [Header("Sensitivity")]//public floats for our x and y sensitivity
     public float sensitivityX = 500f;
     public float sensitivityY = 500f;
-    [Header("Y Rotation Clamp")]
-    //max and min Y rotation
+    [Header("Y Rotation Clamp")]//max and min Y rotation
     public float minimumY = -90.0f;
     public float maximumY = 90.0f;
-    //we will have to invert our mouse position later to calculate our mouse look correctly
-    //float for rotation Y
-    float rotationY = 0.0f;
-    private Vector3 spawnPoint;
     #endregion
     #region Start
     void Start()
     {
-        controller = GetComponent<CharacterController>();
-        myCamera = GameObject.Find("Main Camera");
-        spawnPoint = transform.position;
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
+        controller = GetComponent<CharacterController>();//finds the player controller component
+        myCamera = GameObject.Find("Main Camera");//finds the camera
+        spawnPoint = transform.position;//sets spawnpoint
+        Cursor.lockState = CursorLockMode.Locked;//locks the cursor position
+        Cursor.visible = false;//makes the cursor invisible 
     }
     #endregion
     #region Update
@@ -48,18 +43,19 @@ public class CharacterMovement : MonoBehaviour
         #region Movement
         if (controller.isGrounded)
         {
-            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-            moveDirection = transform.TransformDirection(moveDirection);
-            moveDirection *= speed;
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));//sets the direction the player is going
+            moveDirection = transform.TransformDirection(moveDirection);//makes the player go in a direction
+            moveDirection *= speed;//makes the player go a certain speed
             if (Input.GetButton("Jump"))
             {
-                moveDirection.y = jumpSpeed;
+                moveDirection.y = jumpSpeed;//makes the player jump
             }
         }
-        moveDirection.y -= gravity * Time.deltaTime;
-        controller.Move(moveDirection * Time.deltaTime);
+        moveDirection.y -= gravity * Time.deltaTime;//gives gravity
+        controller.Move(moveDirection * Time.deltaTime);//makes the player move
         #endregion
         #region Axis'
+        //switches between which axis the camera can move
         switch (axis)
         {
             case RotationalAxis.MouseX:
@@ -74,25 +70,31 @@ public class CharacterMovement : MonoBehaviour
         }
         #endregion
         #region Respawn
+        //respawn the player if he fall off the world
         if (transform.position.y < -10)
         {
             transform.position = spawnPoint;
         }
         #endregion
         #region Pausing
-        if (Input.GetKeyDown(KeyCode.Escape) && !isPaused)
+        //pauses the game
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
-            Time.timeScale = 0;
-            isPaused = true;
-            Cursor.visible = true;
-            Cursor.lockState = CursorLockMode.None;
-        }
-        else if (Input.GetKeyDown(KeyCode.Escape) && isPaused)
-        {
-            Time.timeScale = 1;
-            isPaused = false;
-            Cursor.visible = false;
-            Cursor.lockState = CursorLockMode.Locked;
+            if (isPaused)
+            {
+                Time.timeScale = 1;
+                isPaused = false;
+                Cursor.visible = false;
+                Cursor.lockState = CursorLockMode.Locked;
+            }
+            else
+            {
+                Time.timeScale = 0;
+                isPaused = true;
+                Cursor.visible = true;
+                Cursor.lockState = CursorLockMode.None;
+            }
+
         }
         #endregion
     }
@@ -100,46 +102,37 @@ public class CharacterMovement : MonoBehaviour
     #region Axis functons
     void MouseXAndY()
     {
-        if (Time.deltaTime == 0)
+        if (Time.deltaTime == 0)//if the game is paused: lock vertical axis
         {
             rotationY = rotationY;
         }
         else
         {
-            //our rotation Y is plus equals  our mouse input for Mouse Y times Y sensitivity
-            rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+            rotationY += Input.GetAxis("Mouse Y") * sensitivityY;//gets input for y axis
         }
-        //the rotation Y is clamped using Mathf and we are clamping the y rotation to the Y min and Y max
-        rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
-        //transform our local position to the nex vector3 rotaion - y rotaion on the x axis and x rotation on the y axis
-        myCamera.transform.localEulerAngles = new Vector3(-rotationY, 0, 0);
-        
-        transform.Rotate(0, Input.GetAxis("Mouse X") * (sensitivityX*100) * Time.deltaTime, 0);
+        rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);//make the player not break his neck if he looks too far up or down
+        myCamera.transform.localEulerAngles = new Vector3(-rotationY, 0, 0);//sets the cameras vertical direction
+        transform.Rotate(0, Input.GetAxis("Mouse X") * (sensitivityX * 100) * Time.deltaTime, 0);//sets the horizontal direction
     }
     void MouseX()
     {
-
-        //transform the rotation on our game objects Y by our Mouse input Mouse X times X sensitivity
-        transform.Rotate(0, Input.GetAxis("Mouse X") * (sensitivityX * 100) * Time.deltaTime, 0);
+        transform.Rotate(0, Input.GetAxis("Mouse X") * (sensitivityX * 100) * Time.deltaTime, 0);//sets the horizontal direction
     }
     void MouseY()
     {
-        //our rotation Y is pulse equals  our mouse input for Mouse Y times Y sensitivity
-        rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
-        //the rotation Y is clamped using Mathf and we are clamping the y rotation to the Y min and Y max
-        rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
-        //transform our local position to the nex vector3 rotaion - y rotaion on the x axis and local euler angle Y on the y axis
-        myCamera.transform.localEulerAngles = new Vector3(-rotationY, 0, 0);
+        rotationY += Input.GetAxis("Mouse Y") * sensitivityY;//gets input for y axis
+        rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);//make the player not break his neck if he looks too far up or down
+        myCamera.transform.localEulerAngles = new Vector3(-rotationY, 0, 0);//sets the cameras vertical direction
         if (Time.deltaTime == 0)
         {
-            myCamera.transform.localEulerAngles = new Vector3(myCamera.transform.localEulerAngles.y, 0, 0);
+            myCamera.transform.localEulerAngles = new Vector3(myCamera.transform.localEulerAngles.y, 0, 0);//if the game is paused: lock vertical axis
         }
     }
     #endregion
-    
+
 }
 #region RotationalAxis
-public enum RotationalAxis
+public enum RotationalAxis//different possible axis to move the camera
 {
     MouseXAndY,
     MouseX,
--- Assets/Scripts/MenuHandler.cs
+++ Assets/Scripts/MenuHandler.cs
@@ -10,9 +10,14 @@ public class MenuHandler : MonoBehaviour
     #region Variables
     public GameObject mainMenu, optionMenu;
     public bool showOptions;
-    public Slider volSlider, brightSlider;
+    public Slider volSlider, brightSlider, ambientSlider;
+    public Toggle fullScreenToggle;
     public AudioSource mainAudio;
     public Light dirLight;
+    public Vector2[] res = new Vector2[7];
+    public int resIndex;
+    public bool isFullScreen;
+    public Dropdown resDropdown;
     #endregion
 
     public void LoadGame()//starts the game
@@ -33,14 +38,26 @@ public class MenuHandler : MonoBehaviour
             optionMenu.SetActive(false);
             return true;
         }
-        else 
+        else
         {
             showOptions = true;
             mainMenu.SetActive(false);
             optionMenu.SetActive(true);
+
             volSlider = GameObject.Find("Sound Slider").GetComponent<Slider>();
             mainAudio = GameObject.Find("hello world").GetComponent<AudioSource>();
             volSlider.value = mainAudio.volume;
+
+            brightSlider = GameObject.Find("Brightness Slider").GetComponent<Slider>();
+            dirLight = GameObject.Find("Directional Light").GetComponent<Light>();
+            brightSlider.value = dirLight.intensity;
+
+            ambientSlider = GameObject.Find("Ambient Light Slider").GetComponent<Slider>();
+            ambientSlider.value = RenderSettings.ambientIntensity;
+
+            resDropdown = GameObject.Find("Resolution Options").GetComponent<Dropdown>();
+            fullScreenToggle = GameObject.Find("FullScreen Button").GetComponent<Toggle>();
+
             return false;
         }
     }
@@ -50,7 +67,20 @@ public class MenuHandler : MonoBehaviour
     }
     public void Brightness()
     {
-        
+        dirLight.intensity = brightSlider.value;
+    }
+    public void AmbientLight()
+    {
+        RenderSettings.ambientIntensity = ambientSlider.value;
+    }
+    public void Resolution()
+    {
+        resIndex = resDropdown.value;
+        Screen.SetResolution((int)res[resIndex].x, (int)res[resIndex].y, isFullScreen);
+    }
+    public void Windowed()
+    {
+        isFullScreen = fullScreenToggle.isOn;
     }
     public void ExitGame()
     {
