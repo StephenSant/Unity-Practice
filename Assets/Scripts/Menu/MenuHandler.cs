3593d4bbde43dc0c693cf5a134d4f17dff308fe7
StephenSant <42423953+StephenSant@users.noreply.github.com>
2018-08-24 16:07:31 +1000

    24/08/2018
    aaaaaaaaaaaaaaaaaaaaaaaaaAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

---
+++ Assets/Scripts/Menu/MenuHandler.cs
@@ -0,0 +1,151 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;//interacts withe scene changing
+using UnityEngine.UI;//interacting with GUI elements
+using UnityEngine.EventSystems;// BUTTONS!
+namespace Menus
+{
+    public class MenuHandler : MonoBehaviour
+    {
+        #region Variables
+        [Header("Options")]
+        public bool showOptions;
+        public Vector2[] res = new Vector2[7];
+        public int resIndex;
+        public bool isFullScreen;
+        [Header("References")]
+        public Light dirLight;
+        public GameObject mainMenu, optionMenu;
+        public Slider volSlider, brightSlider, ambientSlider;
+        public Toggle fullScreenToggle;
+        public AudioSource mainAudio;
+        public Dropdown resDropdown;
+        [Header("Keys")]
+        public KeyCode holdingKey;
+        public KeyCode forward, backward, left, right, jump, crouch, sprint, interact;
+        [Header("KeyBind References")]
+        public Text forwardText;
+        public Text backwardText, leftText, RightText, jumpText, crouchText, sprintText, interactText;
+        #endregion
+
+        private void Start()
+        {
+            forward = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Forward", "W"));
+            backward = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Backward", "S"));
+            left = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Left", "A"));
+            right = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Right", "D"));
+            jump = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Jump", "Space"));
+            crouch = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Crouch", "LeftControl"));
+            sprint = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Sprint", "LeftShift"));
+            interact = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Interact", "E"));
+        }
+
+        public void LoadGame()//starts the game
+        {
+            SceneManager.LoadScene(1);
+        }
+        public void ToggleOptions()
+        {
+            OptionToggle();
+
+        }
+        bool OptionToggle()
+        {
+            if (showOptions)//showoptions == true;
+            {
+                showOptions = false;
+                mainMenu.SetActive(true);
+                optionMenu.SetActive(false);
+                return true;
+            }
+            else
+            {
+                showOptions = true;
+                mainMenu.SetActive(false);
+                optionMenu.SetActive(true);
+
+                volSlider = GameObject.Find("Sound Slider").GetComponent<Slider>();
+                mainAudio = GameObject.Find("hello world").GetComponent<AudioSource>();
+                volSlider.value = mainAudio.volume;
+
+                brightSlider = GameObject.Find("Brightness Slider").GetComponent<Slider>();
+                dirLight = GameObject.Find("Directional Light").GetComponent<Light>();
+                brightSlider.value = dirLight.intensity;
+
+                ambientSlider = GameObject.Find("Ambient Light Slider").GetComponent<Slider>();
+                ambientSlider.value = RenderSettings.ambientIntensity;
+
+                resDropdown = GameObject.Find("Resolution Options").GetComponent<Dropdown>();
+                fullScreenToggle = GameObject.Find("FullScreen Button").GetComponent<Toggle>();
+
+                return false;
+            }
+        }
+        public void Volume()
+        {
+            mainAudio.volume = volSlider.value;
+        }
+        public void Brightness()
+        {
+            dirLight.intensity = brightSlider.value;
+        }
+        public void AmbientLight()
+        {
+            RenderSettings.ambientIntensity = ambientSlider.value;
+        }
+        public void Resolution()
+        {
+            resIndex = resDropdown.value;
+            Screen.SetResolution((int)res[resIndex].x, (int)res[resIndex].y, isFullScreen);
+        }
+        public void Windowed()
+        {
+            isFullScreen = fullScreenToggle.isOn;
+            Screen.SetResolution((int)res[resIndex].x, (int)res[resIndex].y, isFullScreen);
+        }
+        public void ExitGame()
+        {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#endif
+            Application.Quit();
+            Debug.Log("QUIT!");
+        }
+        public void Save()
+        {
+            PlayerPrefs.SetString("Forward", forward.ToString());
+            PlayerPrefs.SetString("Backward", backward.ToString());
+            PlayerPrefs.SetString("Left", left.ToString());
+            PlayerPrefs.SetString("Right", right.ToString());
+            PlayerPrefs.SetString("Jump", jump.ToString());
+            PlayerPrefs.SetString("Crouch", crouch.ToString());
+            PlayerPrefs.SetString("Spint", sprint.ToString());
+            PlayerPrefs.SetString("Interact", interact.ToString());
+        }
+        private void OnGUI()
+        {
+            Event e = Event.current;
+            if (forward == KeyCode.None)
+            {
+                Debug.Log("KeyCode: " + e.keyCode);
+                if (!(e.keyCode == backward || e.keyCode == left || e.keyCode == right || e.keyCode == jump || e.keyCode == crouch || e.keyCode == sprint || e.keyCode == interact))
+                {
+                    forward = e.keyCode;
+                    holdingKey = KeyCode.None;
+                    forwardText.text = forward.ToString();
+                }
+            }
+        }
+        public void Forward()
+        {
+            if (!(backward == KeyCode.None || left == KeyCode.None || right == KeyCode.None || jump == KeyCode.None || crouch == KeyCode.None || sprint == KeyCode.None || interact == KeyCode.None))
+            {
+                holdingKey = forward;
+                forward = KeyCode.None;
+                forwardText.text = forward.ToString();
+            }
+            
+
+        }
+    }
+}
---
+++ Assets/Scripts/Player/CheckPoint.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+namespace Player
+{
+    //this script can be found in the Component section under the option Character Set Up 
+    //CheckPoint
+    [AddComponentMenu("FirstPerson/CheckPoint")]
+    public class CheckPoint : MonoBehaviour
+    {
+        #region Variables
+        [Header("Check Point Elements")]
+        //GameObject for our currentCheck
+        public GameObject currentCheck;
+        [Header("Character Handler")]
+        //character handler script that holds the players health
+        public CharacterHandler characterHandler;
+        #endregion
+        #region Start
+        //the character handler is the component attached to our player
+        #region Check if we have Key
+        //if we have a save key called SpawnPoint
+        //then our checkpoint is equal to the game object that is named after our save file
+        //our transform.position is equal to that of the checkpoint
+        #endregion
+        #endregion
+        #region Update
+        //if our characters health is less than or equal to 0
+        //our transform.position is equal to that of the checkpoint
+        //our characters health is equal to full health
+        //character is alive
+        //characters controller is active		
+        #endregion
+        #region OnTriggerEnter
+        //Collider other
+        //if our other objects tag when compared is CheckPoint
+        //our checkpoint is equal to the other object
+        //save our SpawnPoint as the name of that object
+        #endregion
+    }
+}
+
+
+
+
+
